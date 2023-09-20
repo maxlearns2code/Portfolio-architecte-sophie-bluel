ec0a2d3e75543185a42c1d087725304827bb68f3
maxlearns2code <maxdevvynter@gmail.com>
2023-09-20 22:15:49 +0200

    show the first project by API

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -2,6 +2,22 @@
 async function loadWorks() {
     const responseWorks = await fetch(`http://localhost:5678/api/works`);
     const works = await responseWorks.json();
-    console.log(works)
+    
+    //show the first project by API
+    const detailsWorks = works[0];    
+
+    const article = document.createElement("figure");
+
+    const imageElement = document.createElement("img");
+    imageElement.src = detailsWorks.imageUrl;
+
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerText = detailsWorks.title;
+
+    const showWorks = document.querySelector(".gallery")
+
+    showWorks.appendChild(article);
+    article.appendChild(imageElement);
+    article.appendChild(titleElement);
 }
 loadWorks()
