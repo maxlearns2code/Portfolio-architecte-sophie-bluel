37aabb9419323d5deb1516feaee5b56f20844bc7
maxlearns2code <maxdevvynter@gmail.com>
2023-09-26 12:04:12 +0200

    create buttons with the filter method

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,46 +1,90 @@
+let projects = []
+
+async function init() {
+    await loadWorks()
+    await loadCategories()
+    resetFilter()
+} 
+init()
+
 //add a function to load the works from the API
 async function loadWorks() {
     const responseWorks = await fetch(`http://localhost:5678/api/works`);
     const works = await responseWorks.json();
+    projects=works
     // show all projects by API
-    for (let i = 0; i < works.length; i++) {
-       //show the first project by API
-        const detailsWorks = works[i]; 
-        
-        const showWorks = document.querySelector(".gallery")
-
-        const article = document.createElement("figure");
-
+    works.forEach((work,id) => {
+        showProjects(work)
+        /*
         const imageElement = document.createElement("img");
         imageElement.src = detailsWorks.imageUrl;
 
         const titleElement = document.createElement("figcaption");
         titleElement.innerText = detailsWorks.title;
+        */
 
-        showWorks.appendChild(article);
+        /*
         article.appendChild(imageElement);
-        article.appendChild(titleElement); 
+        article.appendChild(titleElement);
+        */  
+    });
+    /*
+    for (let i = 0; i < works.length; i++) {
+       //show the first project by API
+        const detailsWorks = works[i];
     }
-    
+    */    
 }
-loadWorks()
 
 //add a function to load the categories from the API
 async function loadCategories() {
     const responseCategories = await fetch(`http://localhost:5678/api/categories`);
     const categories = await responseCategories.json();
-    console.log(responseCategories)
     // show all categories by API
-    for (let j = 0; j < categories.length; j++) {
-        const detailsCategories = categories[j];
-        console.log(detailsCategories)
+    for (let i = 0; i < categories.length; i++) {
+        const detailsCategories = categories[i];
     
         const showCategories = document.querySelector(".filters")
     
         const nameElement = document.createElement("button");
-        nameElement.innerText = detailsCategories.name;    
+        nameElement.innerText = detailsCategories.name;
+        
+        //activate the buttons
+        nameElement.addEventListener("click", () => {
+            //add the filter method
+            const projectsFiltered = projects.filter( (project) => {
+                return project.category.id === detailsCategories.id
+            })
+            const showWorks = document.querySelector(".gallery")
+            //reset HTML elements
+            showWorks.innerHTML = ""
+            //show the projects filtered
+            projectsFiltered.forEach(work => {
+                showProjects(work)
+            });
+        })       
     
-        showCategories.appendChild(nameElement); 
-     }
+        showCategories.appendChild(nameElement);        
+     }    
 }
-loadCategories()
+
+//add a function to show the projects
+function showProjects (work,filter=false) {
+    const showWorks = document.querySelector(".gallery")
+
+    const article = document.createElement("figure");
+    article.innerHTML = `<img src="${work.imageUrl}" max-width="608" max-height="814" alt="${work.title}"><figcaption>${work.title}</figcaption>`
+    showWorks.appendChild(article);
+}
+
+//add a function to reset the filters
+function resetFilter() {
+    const btnAll = document.querySelector(".btnAll")
+    //activate the btn All
+    btnAll.addEventListener("click", () => {
+        const showWorks = document.querySelector(".gallery")
+        //reset HTML elements
+        showWorks.innerHTML = ""
+        loadWorks()
+    })
+}
